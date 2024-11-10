e2888749f696301c859d0c74dbba441aae6f48c3
Vito <vitohartanto@mail.ugm.ac.id>
2024-11-10 20:57:40 +0700

    rm comment

--- src/components/WeatherSlider.js
+++ src/components/WeatherSlider.js
@@ -1,6 +1,3 @@
-
-//               height={50}
-//             />
 //             <p className="text-xl">{hour.temp}°C</p>
 //             <p className="text-sm">{hour.conditions}</p>
 //           </div>
